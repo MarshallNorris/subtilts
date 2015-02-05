a65db6b425ed334f3efab1d9b8230c50bf057c38
MarshallNorris <devmarshallnorris@gmail.com>
2015-02-05 15:36:23 +0000

    Extended FT to test for new user, and URL names

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -38,6 +38,8 @@ class NewVisitorTest(LiveServerTestCase):
         # User hits enter and page updates, displaying
         # "1: Buy peacock feathers"
         inputbox.send_keys(Keys.ENTER)
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, '/lists/.+')
         self.check_for_row_in_list_table('1: Buy peacock feathers')
         
         # Still a text box so user enters "Use peacock feathers to make a fly"
@@ -49,10 +51,29 @@ class NewVisitorTest(LiveServerTestCase):
         self.check_for_row_in_list_table('1: Buy peacock feathers')
         self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
-        # Site has generated unique URL for list and explains that it has
-        self.fail('Finish the test!')        
-
-        # User goes to URL, her to-do list is there
+        # New user visits site
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
         
-        # User leaves
+        # User visits homepage, no old list
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('make a fly', page_text)
 
+        # User starts a new list
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+        
+        # New unique URL
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, '/lists/.+')
+        self.assertNotEqual(francis_list_url, edith_list_url)
+
+        # No trace of old list
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertIn('Buy milk', page_text)
+
+        # User leaves
