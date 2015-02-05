5a21b5fa5374e8cbce2f13035e488836d87e7d60
MarshallNorris <devmarshallnorris@gmail.com>
2015-02-05 11:19:07 +0000

    Extended FT, site can handle single item being added

--- functional_tests.py
+++ functional_tests.py
@@ -36,18 +36,25 @@ class NewVisitorTest(unittest.TestCase):
         
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows),
-            "New to-do item did not appear in table"
-        )
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
 
         # Still a text box so user enters "Use peacock feathers to make a fly"
-        self.fail('Finish the test!')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
         
         # Page updates, shows both items
-        
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn(
+            '2: Use peacock feathers to make a fly',
+            [row.text for row in rows]
+        )    
+
         # Site has generated unique URL for list and explains that it has
-        
+        self.fail('Finish the test!')        
+
         # User goes to URL, her to-do list is there
         
         # User leaves
